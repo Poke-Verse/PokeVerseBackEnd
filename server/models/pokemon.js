96c8d711ef02642fb0143182033801c6fbe796ba
Emil-Deleon-Clover <emil.deleon@clover.com>
2023-03-30 12:33:25 -0400

    Populated Database.

--- server/models/pokemon.js
+++ server/models/pokemon.js
@@ -2,10 +2,10 @@ const { DataTypes } = require("@sequelize/core");
 const { sequelize } = require("../db/db");
 
 const Pokemon = sequelize.define("pokemon", {
-    regionalPokeDexId: DataTypes.INTEGER,
+    regional_pokedex_id: DataTypes.INTEGER,
     name: DataTypes.STRING,
     type: DataTypes.STRING,
-    pokedexEntry: DataTypes.STRING,
+    pokedex_entry: DataTypes.STRING,
     height: DataTypes.INTEGER,
     weight: DataTypes.INTEGER,
     image: DataTypes.STRING,
